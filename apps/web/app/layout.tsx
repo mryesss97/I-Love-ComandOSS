df43a7ef58a6efcfc881f4055a0b2e5c9a0aa948
dangduoc97 <dangvanduoc1997@gmail.com>
2025-06-01 06:36:14 +0700

    update: remove some config relate to UI

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -9,6 +9,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body>
         <Providers>{children}</Providers>
+
       </body>
     </html>
   )
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,7 @@
+
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center bg-blue-100">
-      <div className="bg-red-500 text-white p-4 rounded">Tailwind is working ✅</div>
-    </main>
+    <div>dadas</div>
   )
 }
