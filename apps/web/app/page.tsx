c04a70b260359e5488a73e6c3404b97ab29e2002
dangduoc97 <dangvanduoc1997@gmail.com>
2025-06-01 06:50:10 +0700

    update: config tailwind done

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,6 +2,11 @@
 
 export default function Home() {
   return (
-    <div>dadas</div>
+    <div className="flex min-h-screen flex-col items-center justify-between p-24 bg-red-100">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+
   )
 }
