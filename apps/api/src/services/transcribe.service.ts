8bb9e52d2e25a895022f2be4d76a7cb388afe981
dangduoc97 <dangvanduoc1997@gmail.com>
2025-05-31 22:35:49 +0700

    update: check failed build deploy BE

--- apps/api/src/services/transcribe.service.ts
+++ apps/api/src/services/transcribe.service.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import fs from 'fs';
 import axios from 'axios';
-import FormData from 'form-data';
+// import FormData from 'form-data';
 import { OPENAI_API_KEY } from '../config/env';
 import { compareText } from '../utils/compare';
 
@@ -17,38 +17,38 @@ export const handleTranscribe = async (
   }
 
   // Step 1: Send to Whisper
-  const formData = new FormData();
-  formData.append('file', fs.createReadStream(audioFile.path));
-  formData.append('model', 'whisper-1');
-  formData.append('language', 'en');
-  formData.append('prompt', 'This is an English pronunciation task.');
+  // const formData = new FormData();
+  // formData.append('file', fs.createReadStream(audioFile.path));
+  // formData.append('model', 'whisper-1');
+  // formData.append('language', 'en');
+  // formData.append('prompt', 'This is an English pronunciation task.');
   try {
-    const whisperRes = await axios.post(
-      'https://api.openai.com/v1/audio/transcriptions',
-      formData,
-      {
-        headers: {
-          Authorization: `Bearer ${OPENAI_API_KEY}`,
-          ...formData.getHeaders(),
-        },
-      }
-    );
+    // const whisperRes = await axios.post(
+    //   'https://api.openai.com/v1/audio/transcriptions',
+    //   formData,
+    //   {
+    //     headers: {
+    //       Authorization: `Bearer ${OPENAI_API_KEY}`,
+    //       ...formData.getHeaders(),
+    //     },
+    //   }
+    // );
 
-    const transcribedText = whisperRes?.data?.text;
+    // const transcribedText = whisperRes?.data?.text;
 
     // Step 2: Compare
-    const result = compareText(originalText, transcribedText);
-    console.log('whisperRes ===>>', {
-      whisperRes: whisperRes?.data,
-      originalText,
-      result,
-    });
+    // const result = compareText(originalText, transcribedText);
+    // console.log('whisperRes ===>>', {
+    //   whisperRes: whisperRes?.data,
+    //   originalText,
+    //   result,
+    // });
 
     // Step 3: Return feedback
-    res.json({
-      transcribedText,
-      ...result,
-    });
+    // res.json({
+    //   transcribedText,
+    //   ...result,
+    // });
   } catch (error) {
     console.error(
       'OpenAI Whisper error:',
