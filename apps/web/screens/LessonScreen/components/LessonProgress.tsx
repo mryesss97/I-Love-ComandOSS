edced7f63fdf4fa67155b372c218851f125f6d2c
dangduoc97 <dangvanduoc1997@gmail.com>
2025-06-01 14:57:33 +0700

    update: add navigation to home in LessonProgress component

--- apps/web/screens/LessonScreen/components/LessonProgress.tsx
+++ apps/web/screens/LessonScreen/components/LessonProgress.tsx
@@ -1,14 +1,20 @@
 import React, { useMemo } from 'react'
 import { ArrowLeft } from 'lucide-react';
 import { useLessonContext } from 'apps/web/context/useLessonContext';
+import { useRouter } from 'next/navigation'
 
 
 const LessonProgress = () => {
   const { currentLessonIndex, totalScore, MAX_LESSONS, MAX_SCORE } = useLessonContext()
+  const router = useRouter()
+
+  const backToHome = () => {
+    router.push('/')
+  };
 
   return (
     <div className="grid grid-rows-3 grid-flow-col gap-2">
-      <div>
+      <div onClick={backToHome}>
         <ArrowLeft />
       </div>
       <div >
